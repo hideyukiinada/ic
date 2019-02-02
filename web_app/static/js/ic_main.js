3ce96588c97a8bf4907bedf68b6df15fd7d83078
m <m@ms-Mac-mini.local>
2019-02-02 13:32:28 -0800

    Using wildcard for clearning fields.

--- web_app/static/js/ic_main.js
+++ web_app/static/js/ic_main.js
@@ -18,13 +18,9 @@ $(document).ready(function(){
 
     // Clear button event handler
     $('#clear').click(function() {
-        $("#name1").text("");
-        $("#name2").text("");
-        $("#name3").text("");
-        $("#prob1").text("");
-        $("#prob2").text("");
-        $("#prob3").text("");
-
+        // Clear all fields that contain name and prob in id field.
+        $("[id*=name]").text("");
+        $("[id*=prob]").text("");
         Dropzone.forElement("#myAwesomeDropzone").removeAllFiles(true);
     });
 });
